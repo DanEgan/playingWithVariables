bbfaa7d2416d655109a1e0df24b05eb3adb89d5f
DanEgan <you@example.com>
2020-06-18 15:27:05 -0400

    playing with variables

---
+++ playingWithVariables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace playingWithVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Variable Declarations
+            int age;
+            string name;
+            bool isAlive;
+            //Variable initializations//
+            age = 44;
+            name = "Dan";
+            isAlive = true;
+
+            string dogName = "Gebirah";
+            int dogAge = 13;
+
+            
+            Console.WriteLine($"My dog's name is {dogName}, and she is {dogAge} years old.");
+
+            Console.WriteLine($"{name} {age} is he alive: {isAlive}");
+
+        }
+    }
+}
